299d750ad6e86e0a8d9d06086e710053d44e697a
sandisyd <sandidwi367@gmail.com>
2025-11-30 22:48:55 +0700

    routing in main

--- lib/data/models/task.dart
+++ lib/data/models/task.dart
@@ -4,6 +4,8 @@
 
 import 'dart:convert';
 
+import 'package:tusk_app/data/models/user.dart';
+
 Task taskFromJson(String str) => Task.fromJson(json.decode(str));
 
 String taskToJson(Task data) => json.encode(data.toJson());
@@ -53,9 +55,15 @@ class Task {
         revision: json["revision"],
         dueDate:
             json["dueDate"] == null ? null : DateTime.parse(json["dueDate"]),
-        submitDate: json["submitDate"]  == null ? null : DateTime.parse(json["submitDate"]),
-        rejectedDate: json["rejectedDate"]  == null ? null : DateTime.parse(json["rejectedDate"]),
-        approvedDate: json["approvedDate"] == null ? null : DateTime.parse(json["approvedDate"]),
+        submitDate: json["submitDate"] == null
+            ? null
+            : DateTime.parse(json["submitDate"]),
+        rejectedDate: json["rejectedDate"] == null
+            ? null
+            : DateTime.parse(json["rejectedDate"]),
+        approvedDate: json["approvedDate"] == null
+            ? null
+            : DateTime.parse(json["approvedDate"]),
         attachment: json["attachment"],
         cretedAt:
             json["cretedAt"] == null ? null : DateTime.parse(json["cretedAt"]),
@@ -81,48 +89,5 @@ class Task {
         "cretedAt": cretedAt?.toIso8601String(),
         "updatedAt": updatedAt?.toIso8601String(),
         "user": user?.toJson(),
-      };
-}
-
-class User {
-  final int? id;
-  final String? role;
-  final String? name;
-  final String? email;
-  final String? password;
-  final DateTime? cretedAt;
-  final DateTime? updatedAt;
-
-  User({
-    this.id,
-    this.role,
-    this.name,
-    this.email,
-    this.password,
-    this.cretedAt,
-    this.updatedAt,
-  });
-
-  factory User.fromJson(Map<String, dynamic> json) => User(
-        id: json["id"],
-        role: json["role"],
-        name: json["name"],
-        email: json["email"],
-        password: json["password"],
-        cretedAt:
-            json["cretedAt"] == null ? null : DateTime.parse(json["cretedAt"]),
-        updatedAt: json["updatedAt"] == null
-            ? null
-            : DateTime.parse(json["updatedAt"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "role": role,
-        "name": name,
-        "email": email,
-        "password": password,
-        "cretedAt": cretedAt?.toIso8601String(),
-        "updatedAt": updatedAt?.toIso8601String(),
       };
 }
