b1a18b1291982333eee12936c1d0b193d707b1d8
sandisyd <sandidwi367@gmail.com>
2025-12-06 12:34:50 +0700

    add function delete, get, and insert

--- lib/data/source/user_source.dart
+++ lib/data/source/user_source.dart
@@ -6,6 +6,7 @@ import 'package:tusk_app/data/models/user.dart';
 import 'package:http/http.dart' as http;
 
 class UserSource {
+  ///`'${Url.baseurl}/users'`
   static const String _baseUrl = '${Url.baseurl}/users';
 
   static Future<User?> loginUser(String email, String password) async {
@@ -15,15 +16,54 @@ class UserSource {
             'email': email,
             'password': password,
           }));
-      log("Cek result response : ${response}");
+      log("Cek result response : $response");
       if (response.statusCode == 200) {
         Map result = jsonDecode(response.body);
         return User.fromJson(Map.from(result));
-        log("Cek result body : ${result}");
       }
       return null;
     } catch (e) {
       log("Failed login user : ${e.toString()}");
+      return null;
+    }
+  }
+
+  static Future<bool> insertEmployee(String name, String email) async {
+    try {
+      final response = await http.post(Uri.parse(_baseUrl),
+          body: jsonEncode({
+            'name': name,
+            'email': email,
+          }));
+      return response.statusCode == 201;
+    } catch (e) {
+      log("Failed add employee : ${e.toString()}");
+      return false;
+    }
+  }
+
+  static Future<bool> delete(int userId) async {
+    try {
+      final response = await http.delete(Uri.parse('$_baseUrl/$userId'));
+      log("Success delete user $response");
+      return response.statusCode == 200;
+    } catch (e) {
+      log("Failed deleted user : ${e.toString()}");
+      return false;
+    }
+  }
+
+  static Future<List<User>?> getListEmployee() async {
+    try {
+      final response = await http.get(Uri.parse('$_baseUrl/Employee'));
+      log("Success get users user $response");
+      if (response.statusCode == 200) {
+        List res = jsonDecode(response.body);
+        return res.map((e) => User.fromJson(Map.from(e))).toList();
+      }
+      return null;
+    } catch (e) {
+      log("Failed get list user : ${e.toString()}");
       return null;
     }
   }
